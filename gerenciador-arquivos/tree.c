f202e7ae8a4cb8399dc24c3e6d08da739ebe92bb
Gabriel <152758297+chrusczak@users.noreply.github.com>
2026-05-01 17:40:58 -0400

    Add functions to print and navigate directory path

--- gerenciador-arquivos/tree.c
+++ gerenciador-arquivos/tree.c
@@ -83,3 +83,165 @@ void comando_ma(Nodo *pasta_atual, char *nome){
    }
    
 }
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+// linhas acrescentadas por Gabriel 
+/*
+===========================
+2. IMPRIMIR CAMINHO (AUXILIAR RECURSIVA)
+===========================
+Entrada: Nodo* pasta
+Saída: imprime partes do caminho (sem o "->" final)
+Exemplo:
+-temp-teste2
+Regra:
+- Percorrer via ponteiro pai
+- Ordem: raiz -> ... -> atual
+*/
+void imprimir_caminho_rec(Nodo *pasta){
+
+    if(!pasta || !pasta->pai){
+        return;
+    }
+    imprimir_caminho_rec(pasta->pai);
+    printf("-%s",pasta->nome);
+
+}
+
+/*
+===========================
+1. IMPRIMIR CAMINHO (INTERFACE)
+===========================
+Entrada: Nodo* pasta atual
+Saída: imprime o caminho no formato:
+       -temp-teste2->
+Regra:
+- Se for raiz: imprime apenas "->"
+- Não altera a estrutura
+*/
+void imprimir_caminho(Nodo *pasta){
+    if(!pasta->pai){
+        printf("->");
+        return;
+    }
+    imprimir_caminho_rec( pasta);
+    printf("->");
+
+}
+
+
+
+
+
+/*
+===========================
+3. BUSCAR PASTA
+===========================
+Entrada:
+- Nodo* pasta_atual
+- char* nome
+
+Saída:
+- retorna ponteiro para a pasta encontrada
+- retorna NULL se não existir
+
+Regra:
+- Procurar apenas nós com diretorio == true
+- Percorrer lista: filho -> irmao
+*/
+Nodo* buscar_pasta(Nodo *pasta_atual, char *nome){
+    
+    if(!pasta_atual || !pasta_atual->filho)
+        return NULL;
+
+    Nodo* aux= pasta_atual->filho;
+    while(aux){
+        if(aux->diretorio && strcmp(aux->nome,nome) == 0)
+            return aux;
+        aux = aux->irmao;
+    }
+    return NULL;
+
+}
+
+
+/*
+===========================
+4. COMANDO CD
+===========================
+Entrada:
+- Nodo* pasta_atual
+- char* nome
+
+Saída:
+- retorna nova pasta atual
+- se erro → retorna a mesma pasta
+
+Regras:
+- "cd .." → volta para pai (se existir)
+- "cd nome" → entra na subpasta
+- erro → print "comando invalido"
+*/
+Nodo* comando_cd(Nodo *pasta_atual, char *nome){
+    
+    // tratar o cd ..
+    if(strcmp(nome,"..")==0){
+        if(pasta_atual->pai){
+            return pasta_atual->pai;
+        }else{
+            printf("\ncomando invalido");
+            return pasta_atual;
+        }
+    }
+    // tratar o cd <nome>
+    Nodo* aux = buscar_pasta(pasta_atual,nome);
+    if(!aux){
+        printf("\n comando invalido");   // não encontrou a pasta 
+        return pasta_atual;
+    }
+
+    return aux;
+}
+
+
+/*
+===========================
+5. COMANDO LS
+===========================
+Entrada:
+- Nodo* pasta_atual
+
+Saída:
+- imprime conteúdo da pasta:
+  arquivos → nome
+  pastas   → nome-
+
+Regra:
+- Percorrer lista filho -> irmao
+- Ordem já está garantida na inserção
+*/
+void comando_ls(Nodo *pasta_atual){
+    // TODO:
+    if(!pasta_atual || !pasta_atual->filho){
+        return;
+    }
+    Nodo* aux = pasta_atual->filho;
+    while(aux){
+        if(aux->diretorio){
+            printf("%s-\n",aux->nome);
+        }else{
+            printf("%s\n",aux->nome);
+        }
+        aux = aux->irmao;
+    }
+
+}
